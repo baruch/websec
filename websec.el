101dfd5f3fedce07b55649f692e75c8b5b917c7e
Baruch Even <baruch@ev-en.org>
2003-05-13 04:27:10 +0000

    Add comment in Emacs mode as to its state

--- websec.el
+++ websec.el
@@ -22,6 +22,9 @@
 ;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
+;; This file lost the indentation in the mail archive of websec in yahoo.
+;; It is placed here in the hope that some user will provide a correct version.
+
 ;; Not available in XEmacs
 (require 'generic)
 (require 'font-lock)
